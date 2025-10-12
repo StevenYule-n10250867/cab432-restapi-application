1d2227828a6f63238b2ee797b92afe51fa5fe619
StevenYule-n10250867 <stevenyule050@gmail.com>
2025-10-12 19:22:00 +1000

    Further fixes

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -1,24 +1,36 @@
 // s3.js
 const { S3Client, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+const { SSMClient, GetParameterCommand } = require('@aws-sdk/client-ssm');
 const fs = require('fs');
-const path = require('path');
 
-const s3 = new S3Client({ region: process.env.AWS_REGION });
+const REGION = process.env.AWS_REGION || 'ap-southeast-2';
+const s3 = new S3Client({ region: REGION });
+const ssm = new SSMClient({ region: REGION });
 
-async function uploadFile(localFilePath, key) {
+// helper to load bucket name from Parameter Store if not in env
+async function getBucketName() {
+  if (process.env.AWS_S3_BUCKET) return process.env.AWS_S3_BUCKET;
+  const cmd = new GetParameterCommand({ Name: '/n10250867/AWS_S3_BUCKET' });
+  const res = await ssm.send(cmd);
+  return res.Parameter.Value;
+}
+
+async function uploadFile(localFilePath, key, bucketName) {
+  const finalBucket = bucketName || await getBucketName();
   const fileStream = fs.createReadStream(localFilePath);
   await s3.send(new PutObjectCommand({
-    Bucket: process.env.AWS_S3_BUCKET,
+    Bucket: finalBucket,
     Key: key,
     Body: fileStream
   }));
-  return `s3://${process.env.AWS_S3_BUCKET}/${key}`;
+  return `s3://${finalBucket}/${key}`;
 }
 
-async function downloadFile(key, localFilePath) {
+async function downloadFile(key, localFilePath, bucketName) {
+  const finalBucket = bucketName || await getBucketName();
   const { Body } = await s3.send(new GetObjectCommand({
-    Bucket: process.env.AWS_S3_BUCKET,
+    Bucket: finalBucket,
     Key: key
   }));
   const writeStream = fs.createWriteStream(localFilePath);
@@ -29,20 +41,15 @@ async function downloadFile(key, localFilePath) {
   });
 }
 
-// pre-signed URL helpers
 async function getUploadUrl(key, expiresIn = 3600) {
-  const command = new PutObjectCommand({
-    Bucket: process.env.AWS_S3_BUCKET,
-    Key: key,
-  });
+  const bucket = await getBucketName();
+  const command = new PutObjectCommand({ Bucket: bucket, Key: key });
   return await getSignedUrl(s3, command, { expiresIn });
 }
 
 async function getDownloadUrl(key, expiresIn = 3600) {
-  const command = new GetObjectCommand({
-    Bucket: process.env.AWS_S3_BUCKET,
-    Key: key,
-  });
+  const bucket = await getBucketName();
+  const command = new GetObjectCommand({ Bucket: bucket, Key: key });
   return await getSignedUrl(s3, command, { expiresIn });
 }
 
